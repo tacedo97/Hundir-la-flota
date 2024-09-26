95f13a8563e36fdd8f525dfb927c663448131db4
tacedo97 <tacedo97@gmail.com>
2024-09-27 01:33:46 +0200

    Segundo_Push

--- utils.py
+++ utils.py
@@ -1,39 +1,17 @@
 import numpy as np
 import random
 
-
-
 def crear_tablero(tamaño):
     tablero = np.full((tamaño,tamaño), "_")
     return tablero
 
-
-
-def colocar_barco(barco, tablero):
-    for casilla in barco:
-        tablero[casilla] = "O"
-    return tablero
-
-
-
-def disparar(casilla, tablero):
-    if tablero[casilla] == "O":
-        print("Tocado")
-        tablero[casilla] = "X"
-    else:
-        print("Agua")
-        tablero[casilla] = "A"
-    return tablero
-
-
-
-
-def crear_barco(eslora):
+def crear_barco_dentro(eslora):
     casilla_0 = (random.randint(0,9), random.randint(0,9))
     orientacion = random.choice(["Vertical", "Horizontal"])
 
     barco = [casilla_0]
     casilla = casilla_0
+
     while len(barco) < eslora:
         if orientacion == "Vertical":
             casilla = (casilla[0]+1, casilla[1])
@@ -42,7 +20,20 @@ def crear_barco(eslora):
             casilla = (casilla[0], casilla[1]+1)
             barco.append(casilla) # Horizontal
 
+    for x,y in barco:
+        if x>9 or y>9:
+            barco = crear_barco_dentro(eslora)
     return barco
 
+def colocar_barco(barco, tablero):
+    for casilla in barco:
+        tablero[casilla] = "O"
+    return tablero
 
-#def colocar_barcos(tablero):
+
+def disparar(casilla, tablero):
+    if tablero[casilla] == "O":
+        tablero[casilla] = "X"
+    else:
+        tablero[casilla] = "A"
+    return tablero
